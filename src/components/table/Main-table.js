4d2c3b7189fafbdc920e30043225fd0576f0a237
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-05 17:25:43 +0300

    added sort

--- src/components/table/Main-table.js
+++ src/components/table/Main-table.js
@@ -5,6 +5,8 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Button,
+  Typography,
 } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import './Main-table.css';
@@ -12,32 +14,100 @@ import OneItem from './one-item/One-item';
 
 const MainTable = () => {
   const [users, setUsers] = useState([]);
+  // const [checkedId, setIdChecked] = useState([]);
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((result) => setUsers(result));
   }, []);
+
   const usersList = users.map((item) => {
     const { id, name, email, phone } = item;
     return <OneItem key={id} id={id} name={name} email={email} phone={phone} />;
   });
+
+  const sortList = (criterion) => {
+    const newData = users.slice().sort((a, b) => {
+      if (criterion === 'id') {
+        if (a[criterion] < b[criterion]) return -1;
+        if (a[criterion] > b[criterion]) return 1;
+        return 0;
+      } else {
+        if (a[criterion].toLowerCase() < b[criterion].toLowerCase()) return -1;
+        if (a[criterion].toLowerCase() > b[criterion].toLowerCase()) return 1;
+        return 0;
+      }
+    });
+    setUsers(newData);
+  };
+
+  const sortIdHandler = () => {
+    sortList('id');
+  };
+
+  const sortNameHandler = () => {
+    sortList('name');
+  };
+
+  const sortEmailHandler = () => {
+    sortList('email');
+  };
+  const sortPhoneHandler = () => {
+    sortList('phone');
+  };
+
+  const onDelete = (id) => {
+    const newData = users.slice().filter((item) => item.id !== id);
+    setUsers(newData);
+  };
+
+  const onDeleteHandler = () => {
+    onDelete(3);
+  };
+
   return (
-    <>
-      <h2 className="main-title">Table of users</h2>
-      <TableContainer className="table">
+    <div className="table">
+      <Typography variant="h2" className="main-title">
+        Table of users
+      </Typography>
+      <TableContainer>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="center">id</TableCell>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Email</TableCell>
-              <TableCell align="center">Phone</TableCell>
+              <TableCell align="center">
+                <Button className="btn__title" onClick={sortIdHandler}>
+                  id
+                </Button>
+              </TableCell>
+              <TableCell align="center">
+                <Button className="btn__title" onClick={sortNameHandler}>
+                  Name
+                </Button>
+              </TableCell>
+              <TableCell align="center">
+                <Button className="btn__title" onClick={sortEmailHandler}>
+                  Email
+                </Button>
+              </TableCell>
+              <TableCell align="center">
+                <Button className="btn__title" onClick={sortPhoneHandler}>
+                  Phone
+                </Button>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>{usersList}</TableBody>
         </Table>
       </TableContainer>
-    </>
+      <Button
+        variant="contained"
+        color="secondary"
+        className="btn__delete"
+        onClick={onDeleteHandler}
+      >
+        Delete
+      </Button>
+    </div>
   );
 };
 
--- src/components/table/one-item/One-item.js
+++ src/components/table/one-item/One-item.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import { TableCell, TableRow } from '@material-ui/core';
+import { TableCell, TableRow, Checkbox } from '@material-ui/core';
+import { useState } from 'react';
 
 const OneItem = (props) => {
-    const {id, name, email, phone} = props
+  const { id, name, email, phone } = props;
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
   return (
     <TableRow>
-      <TableCell align="center">{id}</TableCell>
+      <TableCell align="center">
+        <Checkbox checked={checked} onChange={handleChange} />
+        {id}
+      </TableCell>
       <TableCell align="center">{name}</TableCell>
       <TableCell align="center">{email}</TableCell>
       <TableCell align="center">{phone}</TableCell>
