49751ac779fad2115e2a6e65db704feb80cf5fbb
Polina Raguilo <polinar2007@mail.ru>
2021-04-09 13:58:07 +0300

    Created function to add new user (with json-server)

--- src/components/add-user-form/add-user-form.js
+++ src/components/add-user-form/add-user-form.js
@@ -1,39 +1,85 @@
 import { Button, Container, TextField, Typography } from '@material-ui/core';
 import '@/components/add-user-form/add-user-form.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../config/constants';
+import { useDispatch } from 'react-redux';
+import { fethcUsers } from '../../redux/actions/usersAction';
+import { nanoid } from 'nanoid';
 
 const AddUserForm = () => {
+  const [newUser, setNewUser] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+  const dispatch = useDispatch();
+  const newId = nanoid(5);
+  const onInputChange = (e) => {
+    const { id, value } = e.target;
+    setNewUser({
+      ...newUser,
+      [id]: value,
+    });
+  };
+  const addUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${API_URL}/users`, {
+        id: newId,
+        ...newUser,
+      });
+      setNewUser({ name: '', email: '', phone: '' });
+      dispatch(fethcUsers());
+      alert('Success!');
+    } catch (err) {
+      console.log(err);
+      alert('Try again!');
+    }
+  };
   return (
     <div className="add-wrapper">
       <Typography variant="h4" className="add-title">
         Add new user
       </Typography>
-      <form action="submit">
+      <form action="submit" onSubmit={addUser}>
         <Container>
           <TextField
-            id="Name"
+            id="name"
             label="Name"
             variant="outlined"
             className="add-input"
+            value={newUser.name}
+            onChange={onInputChange}
           />
         </Container>
         <Container>
           <TextField
-            id="Email"
+            id="email"
             label="Email"
             variant="outlined"
             className="add-input"
+            value={newUser.email}
+            onChange={onInputChange}
           />
         </Container>
         <Container>
           <TextField
-            id="Phone"
+            id="phone"
             label="Phone"
             variant="outlined"
             className="add-input"
+            value={newUser.phone}
+            onChange={onInputChange}
           />
         </Container>
-        <Button variant="contained" color="secondary" className="add-btn">
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          className="add-btn"
+        >
           Add
         </Button>
       </form>
