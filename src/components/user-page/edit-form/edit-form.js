9c4044a6f3bca0869d79f69581e6cfea7d6e686a
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-14 16:20:09 +0300

    Added validation for edit-form

--- src/components/user-page/edit-form/edit-form.js
+++ src/components/user-page/edit-form/edit-form.js
@@ -1,37 +1,28 @@
 import { Button, Container, TextField, Typography } from '@material-ui/core';
 import '@/components/user-page/edit-form/edit-form.css';
-import { useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '@/config/constants';
 import { useDispatch } from 'react-redux';
 import { updateUsers } from '@/redux/actions/usersAction';
+import { Controller, useForm } from 'react-hook-form';
 
 const EditForm = (props) => {
-  const dispatch = useDispatch();
-
   const { name: userName, email, phone } = props.currentUser;
 
-  const [newInformation, setNewInformation] = useState({
-    userName,
-    email,
-    phone,
-  });
+  const dispatch = useDispatch();
 
-  const onChangeInput = (e) => {
-    const { id, value } = e.target;
-    setNewInformation({
-      ...newInformation,
-      [id]: value,
-    });
-  };
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { userName, email, phone } });
 
-  const onUpdateHandler = async (e) => {
-    e.preventDefault();
+  const onUpdateHandler = async (data) => {
     const newData = {
       ...props.currentUser,
-      name: newInformation.userName,
-      email: newInformation.email,
-      phone: newInformation.phone,
+      name: data.userName,
+      email: data.email,
+      phone: data.phone,
     };
     try {
       await axios.put(`${API_URL}/users/${props.currentUser.id}`, newData);
@@ -46,31 +37,62 @@ const EditForm = (props) => {
       <Typography variant="h4" className="edit__title">
         Edit user
       </Typography>
-      <form className="edit__form" onSubmit={onUpdateHandler}>
-        <TextField
-          variant="outlined"
-          label="Name"
-          className="edit__input"
-          id="userName"
-          onChange={onChangeInput}
-          value={newInformation.userName}
+      <form className="edit__form" onSubmit={handleSubmit(onUpdateHandler)}>
+        <Controller
+          name="userName"
+          render={({ field }) => (
+            <TextField
+              error={errors.userName ? true : false}
+              variant="outlined"
+              label="Name"
+              className="edit__input"
+              helperText={errors.userName && 'Name is required!'}
+              {...field}
+            />
+          )}
+          control={control}
+          rules={{ required: true }}
+          defaultValue={userName}
         />
-        <TextField
-          variant="outlined"
-          label="Email"
-          className="edit__input"
-          id="email"
-          onChange={onChangeInput}
-          value={newInformation.email}
+        <Controller
+          name="email"
+          render={({ field }) => (
+            <TextField
+              error={errors.email ? true : false}
+              variant="outlined"
+              label="Email"
+              className="edit__input"
+              helperText={errors.email && 'Invalid email!'}
+              {...field}
+            />
+          )}
+          control={control}
+          rules={{
+            required: true,
+            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+          }}
+          defaultValue={email}
         />
-        <TextField
-          variant="outlined"
-          label="Phone"
-          className="edit__input"
-          id="phone"
-          onChange={onChangeInput}
-          value={newInformation.phone}
+        <Controller
+          name="phone"
+          render={({ field }) => (
+            <TextField
+              error={errors.phone ? true : false}
+              variant="outlined"
+              label="Phone"
+              className="edit__input"
+              helperText={errors.phone && 'Invalid phone! Only numbers'}
+              {...field}
+            />
+          )}
+          control={control}
+          rules={{
+            required: true,
+            pattern: /^[0-9]+$/,
+          }}
+          defaultValue={phone}
         />
+
         <Button
           color="secondary"
           variant="contained"
