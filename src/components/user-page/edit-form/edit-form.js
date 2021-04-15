75fb3849fb7025692c1efb910c41e4ed0982bf6e
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-15 11:02:59 +0300

    Created component for Controller

--- src/components/user-page/edit-form/edit-form.js
+++ src/components/user-page/edit-form/edit-form.js
@@ -1,10 +1,11 @@
-import { Button, Container, TextField, Typography } from '@material-ui/core';
+import { Button, Container, Typography } from '@material-ui/core';
 import '@/components/user-page/edit-form/edit-form.css';
 import axios from 'axios';
 import { API_URL } from '@/config/constants';
 import { useDispatch } from 'react-redux';
 import { updateUsers } from '@/redux/actions/usersAction';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import FormController from '@/components/form-checking-controller/form-checking-controller';
 
 const EditForm = (props) => {
   const { name: userName, email, phone } = props.currentUser;
@@ -38,59 +39,38 @@ const EditForm = (props) => {
         Edit user
       </Typography>
       <form className="edit__form" onSubmit={handleSubmit(onUpdateHandler)}>
-        <Controller
+        <FormController
+          err={errors.userName}
+          defaultParam={userName}
+          labelName="Name"
+          textHelp="Name is required!"
+          rulesValidation={{ required: true }}
           name="userName"
-          render={({ field }) => (
-            <TextField
-              error={errors.userName ? true : false}
-              variant="outlined"
-              label="Name"
-              className="edit__input"
-              helperText={errors.userName && 'Name is required!'}
-              {...field}
-            />
-          )}
           control={control}
-          rules={{ required: true }}
-          defaultValue={userName}
         />
-        <Controller
-          name="email"
-          render={({ field }) => (
-            <TextField
-              error={errors.email ? true : false}
-              variant="outlined"
-              label="Email"
-              className="edit__input"
-              helperText={errors.email && 'Invalid email!'}
-              {...field}
-            />
-          )}
-          control={control}
-          rules={{
+        <FormController
+          err={errors.email}
+          defaultParam={email}
+          labelName="Email"
+          textHelp="Email is required!"
+          rulesValidation={{
             required: true,
             pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
           }}
-          defaultValue={email}
-        />
-        <Controller
-          name="phone"
-          render={({ field }) => (
-            <TextField
-              error={errors.phone ? true : false}
-              variant="outlined"
-              label="Phone"
-              className="edit__input"
-              helperText={errors.phone && 'Invalid phone! Only numbers'}
-              {...field}
-            />
-          )}
+          name="email"
           control={control}
-          rules={{
+        />
+        <FormController
+          err={errors.phone}
+          defaultParam={phone}
+          labelName="Phone"
+          textHelp="Invalid phone! Only numbers"
+          rulesValidation={{
             required: true,
             pattern: /^[0-9]+$/,
           }}
-          defaultValue={phone}
+          name="phone"
+          control={control}
         />
 
         <Button
